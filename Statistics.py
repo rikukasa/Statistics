4269a0c6f3fd7c59903f56c141da0cc91dbe6fe5
rikukasa <80609650+rikukasa@users.noreply.github.com>
2021-11-05 19:18:29 +0900

    1

--- Statistics.py
+++ Statistics.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+f = 2
+
+x = np.array([i for i in range(0,360 * f)])
+s = np.array([np.sin(2 * np.pi* i / 360) for i in x])
+c = np.array([np.cos(2 * np.pi* i / 360) for i in x])
+
+plt.plot(x, s, color = "r")
+plt.plot(x, c, color = "b")
+plt.show()
